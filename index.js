f5a9541f28c2ca5983475bc8e85b518d3b0d604b
Ryan Sanford <ryansanford@flywheel.io>
2016-12-14 09:26:13 -0600

    Handle disconnects to the LDAP server reliably

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ var authenticate = function (username, password) {
 };
 
 app.post('/authenticate', function (req, res) {
-    if(req.body.username && req.body.password) {
-        authenticate(req.body.username, req.body.password)
+	if(req.body.username && req.body.password) {
+		authenticate(req.body.username, req.body.password)
 			.then(function(user) {
 				var expires = moment().add(2, 'days').valueOf();
 				var token = jwt.encode({
@@ -44,12 +44,29 @@ app.post('/authenticate', function (req, res) {
 				res.json({token: token, full_name: user.cn});
 			})
 			.catch(function (err) {
+				// Ldap reconnect config needs to be set to true to reliably
+				// land in this catch when the connection to the ldap server goes away.
+				// REF: https://github.com/vesse/node-ldapauth-fork/issues/23#issuecomment-154487871
+
 				console.log(err);
-				res.status(401).send({ error: 'Wrong user or password'});
+
+				if (err.name === 'InvalidCredentialsError') {
+					res.status(401).send({ error: 'Wrong user or password'});
+				} else {
+					// ldapauth-fork or underlying connections may be in an unusable state.
+					// Reconnect option does re-establish the connections, but will not
+					// re-bind. Create a new instance of LdapAuth.
+					// REF: https://github.com/vesse/node-ldapauth-fork/issues/23
+					// REF: https://github.com/mcavage/node-ldapjs/issues/318
+
+					res.status(500).send({ error: 'Unexpected Error'});
+					auth = new LdapAuth(settings.ldap);
+				}
+
 			});
-    } else {
-        res.status(400).send({error: 'No username or password supplied'});
-    }
+		} else {
+			res.status(400).send({error: 'No username or password supplied'});
+		}
 });
 
 app.post('/verify', function (req, res) {
@@ -78,6 +95,9 @@ app.post('/verify', function (req, res) {
 
 var port = (process.env.PORT || 3000);
 app.listen(port, function() {
-    console.log('Listening on port: ' + port);
-});
+	console.log('Listening on port: ' + port);
 
+	if (typeof settings.ldap.reconnect === 'undefined' || settings.ldap.reconnect === null || settings.ldap.reconnect === false) {
+		console.warn('WARN: This service may become unresponsive when ldap reconnect is not configured.')
+	}
+});
